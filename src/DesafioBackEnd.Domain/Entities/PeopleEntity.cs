a0612579e3e7025aa334dcd3c86d7fe734016290
paulo ricardo t <spacetechinformatica@gmail.com>
2020-08-21 00:19:26 -0300

    Add project files.

---
+++ src/DesafioBackEnd.Domain/Entities/PeopleEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioBackEnd.Domain.Entities
+{
+    public class PeopleEntity
+    {
+
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public string Gender { get; private set; }
+
+        public PeopleEntity(string firstName, string lastName, string email, string phone, string gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            Gender = gender;
+        }
+
+
+    }
+}
